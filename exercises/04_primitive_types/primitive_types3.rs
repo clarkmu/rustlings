93ce784c85309322e7153687a79e8939b3e93e10
Michael <clarkmu@gmail.com>
2023-12-11 19:28:59 -0500

    chapters 1 through 6

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,12 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let mut a = [0,1,2,3];
 
-    if a.len() >= 100 {
+    if a.len() >= 2 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
